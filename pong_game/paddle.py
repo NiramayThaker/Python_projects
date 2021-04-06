a6de3a070d97f8790db5087ca716763d847fb9c9
Niramay Thaker <70151582+Niramay33@users.noreply.github.com>
2021-04-06 19:20:07 +0530

    Creating 2 paddles and making it move

--- pong_game/paddle.py
+++ pong_game/paddle.py
@@ -10,12 +10,6 @@ class Paddle(Turtle):
         self.shapesize(stretch_len=1, stretch_wid=5)
         self.goto(position)
 
-    # def create_line(self):
-    #     self.color("White")
-    #     self.left(90)
-    #     self.goto(x=0, y=240)
-    #     self.goto(x=0, y=-290)
-
     def go_up(self):
         new_y = self.ycor() + 40
         self.goto(self.xcor(), new_y)
