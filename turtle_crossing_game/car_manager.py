0d76a805e0b17300579c0e8a471678029d133a74
Niramay Thaker <70151582+Niramay33@users.noreply.github.com>
2021-04-10 08:58:33 +0530

    It will control the flow of car

---
+++ turtle_crossing_game/car_manager.py
@@ -0,0 +1,20 @@
+from turtle import Turtle
+import random
+
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
+
+
+class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("square")
+        self.shapesize(stretch_wid=1, stretch_len=2)
+        self.penup()
+        self.goto(230, 0)
+        self.color(random.choice(COLORS))
+
+    def move_car(self):
+        self.ycor() + STARTING_MOVE_DISTANCE
+
