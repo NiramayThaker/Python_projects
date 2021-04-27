ae6af5186fee4150bb124a4c7548485d8a1de0a8
Niramay Thaker <70151582+Niramay33@users.noreply.github.com>
2021-04-27 08:08:31 +0530

    Just run it and send mail

---
+++ automated_mail_sender_for_monday_motivation/main.py
@@ -0,0 +1,25 @@
+import datetime as dt
+import random
+import smtplib
+
+MY_EMAIL = "xyz@gmail.com"  # Enter your gmail account
+MY_PASSWORD = "$*#8"   # Enter your password
+SEND_TO = "abc@gmail.com"   # Enter Account of person you want to send
+
+now = dt.datetime.now()
+weekday = now.weekday()
+if weekday == 1:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+
+    print(quote)
+    # server = smtplib.SMTP('smtp.gmail.com', 587)
+    with smtplib.SMTP('smtp.gmail.com', 587) as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=SEND_TO,
+            msg=f"Subject:Sending Tuesday Motivation from pycharm\n\n{quote}"
+        )
